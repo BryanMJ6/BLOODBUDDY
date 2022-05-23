2864a7735ae239e22f7720081099eb0f0f589bd3
BryanMJ6 <89365620+BryanMJ6@users.noreply.github.com>
2022-05-23 03:28:14 -0500

    Add files via upload

---
+++ resources/views/equipos/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('template_title')
+equipos
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center"></div>
+    <br>
+    <br>
+    <br>
+
+    <h1 class="text-5xl text-center pt-14 text-danger"> ¿Quienes Somos?</h1>
+    <br>
+    <br>
+    <h1 class="text-5xl text-center pt-14 text-dark">Somos personas que buscan brindar una herramienta util para
+        la donacion de sangre fijandonos en ser una de mucha utilidad
+        en un futuro para las personas.</h1>
+
+    <br>
+    <br>
+    <br>
+
+    <div class="row justify-content-center">
+        <a href="http://127.0.0.1:8000/" class="btn btn-outline-dark">Volver ala Pagina Principal</a>
+
+        <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </div>
+</div>
+</div>
+@endsection
