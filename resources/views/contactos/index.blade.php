1a4c02f272899ec1e083eceb8de45066f6a2988e
BryanMJ6 <89365620+BryanMJ6@users.noreply.github.com>
2022-05-23 03:25:59 -0500

    Add files via upload

---
+++ resources/views/contactos/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('template_title')
+contactos
+@endsection
+
+@section('content')
+<div class="col-sm-12">
+    <div class="container">
+        <div class="row justify-content-center"></div>
+        <h1 class="text-5xl text-center pt-14 text-danger">Informacion de Contacto</h1>
+        <h1 class="text-5xl text-center pt-14">aqui podra acceder a los distintos medios de comunicacion que tenemos
+            para cualquier tipo de consultas que se le presenten.</h1>
+
+        <div class="row justify-content-center">
+            <h1 class="text-5xl text-center pt-14">Contactanos atraves de estos medios.</h1>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <a target="_blank" class=" text-5xl text-center pt-14 btn btn-outline-success"
+                href="https://www.whatsapp.com/">Nuestro
+                Whatsapp</a>
+
+
+            <a target="_blank" class="text-5xl text-center pt-14 btn btn-outline-primary"
+                href="https://www.facebook.com/">Nuestro
+                Facebook</a>
+
+
+            <a target="_blank" class="text-5xl text-center pt-14 btn btn-outline-danger"
+                href="https://www.instagram.com/">Nuestro
+                instagram</a>
+
+
+            <a target="_blank" class="text-5xl text-center pt-14 btn btn-outline-info"
+                href="https://web.telegram.org/z/">Nuestro Telegram</a>
+
+            <th></th>
+            </tr>
+            </thead>
+
+            <body>
+                <link rel=" stylesheet" href="css/styles.css">
+                <div class="fondo">
+                    </form>
+                    </td>
+                    </tr>
+
+            </body>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+@endsection
